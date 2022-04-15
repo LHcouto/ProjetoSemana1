b57e6b6287f84e2a68148cad5f297336a11c735a
LHcouto <xluisx_couto@hotmail.com>
2022-04-15 13:26:42 -0300

    feat: adc GET-ALL

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 const express = require('express');
+const cors = require('cors');
 const port = 3000;
 const app = express();
 
 // Configurando nossa aplicação para trabalhar com JSON's
 app.use(express.json());
-
+app.use(cors());
 // Definir dados para usar na aplicação
-const paletas = [
+const computadores = [
   {
     id: 1,
-    sabor: 'Açai com Leite Condensado',
-    descricao: 'Açai com Leite Condensado',
-    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-    preco: 10.0,
+    nome: 'Computador Gamer Uller',
+    descricao: 'NTEL I5-3470, GEFORCE GTX 1650 4GB, 8GB DDR3, SSD 240GB',
+    foto: 'assets/img/computador1.png',
+    preco: 3.470,
   },
   {
     id: 2,
-    sabor: 'Banana com Nutella',
-    descricao: 'Banana com Nutella',
+    nome: 'Computador Gamer Bonnacon',
+    descricao: 'INTEL I5-3470, RADEON RX 550 4GB, 8GB DDR3, SSD 240GB',
     foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-    preco: 10.0,
+    preco: 2.722,
   },
   {
     id: 3,
-    sabor: 'Chocolate Belga',
-    descricao: 'Chocolate Belga',
+    sabor: 'Computador Gamer Zagan',
+    descricao: 'INTEL I5-3470, GEFORCE GT 1030 2GB, 8GB DDR3, SSD 240GB',
     foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-    preco: 7.0,
+    preco: 2.495,
   },
+  {
+    id: 4,
+    sabor: 'Computador Gamer Arcadia',
+    descricao: 'NTEL I5-3570, RADEON RX 550 4GB, 8GB DDR3, SSD 240GB',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 2.719,
+  },
+
 ];
 
-app.get('/', function (req, res) {
-  res.send('Hello Blue Módulo 3 Fullstack');
-});
-
-app.get('/paletas/find-paletas', (req, res) => {
-  res.send(paletas);
+app.get('/computadores/todos-computadores', (req, res) => {
+  res.send(computadores);
 });
 
 app.listen(port, () => {
